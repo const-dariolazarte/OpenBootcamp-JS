d5fef5eab0c054e5792d2d2f26a715096ba9cde9
const-dariolazarte <dariolazarte1985@gmail.com>
2022-08-26 19:53:50 -0300

    done

---
+++ 07 sets y objetos/conjuntos.js
@@ -0,0 +1,12 @@
+// Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+
+// - Un nuevo Set con los nombres de tu familia
+
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+
+// - Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;
+
+const myFamily = new Set(['Dario', 'Fabiana', 'Ahri']);
+
+myFamily.add('Dario');
+myFamily.add('JavaScript')
