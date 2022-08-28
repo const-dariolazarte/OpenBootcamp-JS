ea04ec2cb5a0f71fc9bcb1dd23c5e6cb5dd7c90b
const-dariolazarte <dariolazarte1985@gmail.com>
2022-08-27 22:32:04 -0300

    done

--- 09 errores/index.js
+++ 09 errores/index.js
@@ -6,26 +6,23 @@
 
 // - Registra el error en un archivo a través de un try...catch
 
-const winston = require('winston');
+const winston = require("winston");
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: "error",
   format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: "user-service" },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-  
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
   ],
 });
 
-function returnError() {
-  return logger.error('Este es un mensaje de error personalizado');
+function showError() {
+  throw new Error("showError function");
 }
 
-returnError();
+try {
+  showError();
+} catch (e) {
+  logger.log("error", e.toString());
+}
